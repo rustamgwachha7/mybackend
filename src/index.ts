c4b8ebf2da20d3254b7654a181d222bf3f27ece9
rustam <invadeluffy@gmail.com>
2026-08-17 17:03:42 +0545

    Set up Express + TypeScript backend with dotenv config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
