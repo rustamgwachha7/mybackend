660ffb87ce307921a3da4788bf4173d1825e24f2
rustam <invadeluffy@gmail.com>
2026-08-19 21:32:13 +0545

    final

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "./config/env";
 
 import express from "express";
+import cors from "cors";
 import taskroutes from "./routes/task.routes";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
